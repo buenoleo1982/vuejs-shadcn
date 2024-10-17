7e5c0325512dafd3fa5e0ad11506b201207fc7a3
Leo Bueno <leonardo@buenoinfo.net>
2024-10-17 17:23:48 -0300

    🔧 Add proxy for /graphql

--- vite.config.ts
+++ vite.config.ts
@@ -9,53 +9,66 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import VueRouter from 'unplugin-vue-router/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Layouts from 'vite-plugin-vue-layouts'
 
-// https://vite.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
     },
-  },
-  plugins: [
-    VueRouter({
-      extensions: ['.vue', '.md'],
-      dts: 'src/typed-router.d.ts',
-    }),
-    Layouts(),
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-i18n',
-        VueRouterAutoImports,
-        {
-          'vue-router/auto': ['useLink'],
+    plugins: [
+      VueRouter({
+        extensions: ['.vue', '.md'],
+        dts: 'src/typed-router.d.ts',
+      }),
+      Layouts(),
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-i18n',
+          VueRouterAutoImports,
+          {
+            'vue-router/auto': ['useLink'],
+          },
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables', 'src/stores'],
+        vueTemplate: true,
+      }),
+      Components({
+        extensions: ['vue', 'md'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: 'src/components.d.ts',
+      }),
+      VueI18n({
+        runtimeOnly: true,
+        compositionOnly: true,
+        fullInstall: true,
+        include: [path.resolve(__dirname, 'locales/**')],
+      }),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+        '/graphl': {
+          target: `${env.VITE_API_URL ?? 'http://localhost:8787'}`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: path => path.replace(/^\/graphl/, ''),
         },
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/composables', 'src/stores'],
-      vueTemplate: true,
-    }),
-    Components({
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
-    }),
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      fullInstall: true,
-      include: [path.resolve(__dirname, 'locales/**')],
-    }),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
